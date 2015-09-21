7e60640a61b4e8a3f5e827a54f0491349d02f125
ing-raf <dott.ing.raffaele.martino@gmail.com>
2015-09-21 12:42:37 +0200

    Added specific Gestore interface for Android devices

---
+++ src/Server/RMIInterface/ServiziGestoreAndroid.java
@@ -0,0 +1,41 @@
+package Server.RMIInterface;
+
+import java.util.ArrayList;
+
+public interface ServiziGestoreAndroid {
+
+	ArrayList<? extends Autovettura> retrieveListaModelli() throws Exception;
+
+	/**
+	 * 
+	 * @param IDbatteria
+	 * @param costosostituzione
+	 * @param maxciclidiricarica
+	 * @param modelloautovettura
+	 */
+	boolean addBatteria(int IDbatteria, float costosostituzione, int maxciclidiricarica, int modelloautovettura) throws Exception;
+
+	/**
+	 * 
+	 * @param IDstazione
+	 * @param listabatterie
+	 */
+	ArrayList<? extends Batteria> retrieveBatterieQuasiEsauste(int IDstazione) throws Exception;
+
+	/**
+	 * 
+	 * @param nome
+	 * @param cognome
+	 * @param datanascita
+	 */
+	ArrayList<? extends AutovetturaCliente> retrieveAutovettureCliente(int codicebadge) throws Exception;
+
+	/**
+	 * 
+	 * @param modello
+	 */
+	ArrayList<? extends Stazione> remoteRetrieveBatterieCompatibili(int modello) throws Exception;
+
+	Sostituzione retrieveUltimaSostituzione(int autovettura) throws Exception;
+
+}
