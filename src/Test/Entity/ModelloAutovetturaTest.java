4e4c5894c4a0dfefb51bf875b2909efa3b544a90
MariaLombardi <luciellamariella@gmail.com>
2015-10-02 11:42:53 +0200

    test di modelloAutovettura

--- src/Test/Entity/ModelloAutovetturaTest.java
+++ src/Test/Entity/ModelloAutovetturaTest.java
@@ -8,57 +8,75 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import Server.DAO.ModelloAutovetturaDAO;
+import Server.DAO.PopulateTestDatabase;
+import Server.DAO.StazioneDAO;
+import Server.Entity.ModelloAutovettura;
+import Server.Entity.Stazione;
+
 public class ModelloAutovetturaTest {
+
+	private ModelloAutovettura test = null;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+		PopulateTestDatabase.populate();
 	}
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void testModelloAutovettura() {
-		fail("Not yet implemented");
+		this.test = new ModelloAutovettura();
+		test.setBrand("Peugeot");
+		test.setModel("207");		
 	}
 
 	@Test
 	public void testModelloAutovetturaModelloAutovetturaDAO() {
-		fail("Not yet implemented");
+		final String idTest = "Test del costruttore dal DAO";
+		
+		ModelloAutovetturaDAO dao = ModelloAutovetturaDAO.findModelloAuto("207", "Peugeot");
+		ModelloAutovettura nuova = new ModelloAutovettura(dao);
+		
+		assertEquals(idTest + " riuscito", test, nuova);
 	}
 
 	@Test
 	public void testGetBrand() {
-		fail("Not yet implemented");
+		final String idTest = "Test di getBrand";
+		
+		assertEquals(idTest + " riuscito", "Peugeot", test.getBrand());
 	}
 
 	@Test
 	public void testSetBrand() {
-		fail("Not yet implemented");
+		final String idTest = "Test di setBrand";
+		
+		test.setBrand("Fiat");
+		assertEquals(idTest + "riuscito", "Fiat", test.getBrand());
 	}
 
 	@Test
 	public void testGetModel() {
-		fail("Not yet implemented");
+		final String idTest = "Test di getModel";
+		
+		assertEquals(idTest + " riuscito", "207", test.getModel());
 	}
 
 	@Test
 	public void testSetModel() {
-		fail("Not yet implemented");
+		final String idTest = "Test di setModel";
+		
+		test.setModel("Panda");
+		assertEquals(idTest + "riuscito", "Panda", test.getModel());
 	}
 
 	@Test
 	public void testPrepareDAO() {
-		fail("Not yet implemented");
+		final String idTest = "Test di prepareDAO";
+		
+		ModelloAutovetturaDAO dao = test.prepareDAO();
+		assertEquals(idTest + "riuscito", ModelloAutovetturaDAO.findModelloAuto("207", "Peugeot"), dao);
+		
 	}
 
 }
